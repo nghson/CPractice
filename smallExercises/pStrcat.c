bf13ca765afeebadc5b84cd62c19775c26ae96ac
nghson <son.hngy@gmail.com>
2023-08-07 21:32:00 +0300

    finished pointer strcat

--- smallExercises/pStrcat.c
+++ smallExercises/pStrcat.c
@@ -1,4 +1,21 @@
+#include <stdio.h>
+
+void p_strcat(char *s, char *t)
+{
+	for (; *s != '\0'; ++s) {
+		;
+	}
+	for (; (*(s++) = *(t++)) != '\0';) {
+		;
+	}
+}
+
 int main()
 {
+	char s[100] = "Hello, ";
+	char t[] = "World!";
+	p_strcat(s, t);
+
+	printf("The concatenated string: %s\n", s);
 	return 0;
 }
